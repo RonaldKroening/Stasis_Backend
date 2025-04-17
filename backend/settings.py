a176b8328bb3aaf46c4e82a57a6fc86f7edf97d3
Ronald Kroening <rkroen01@tufts.edu>
2025-04-17 11:37:52 -0400

    fix sttng 2

--- backend/settings.py
+++ backend/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os,.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-1'  
 
 WSGI_APPLICATION = 'backend.wsgi.application'
